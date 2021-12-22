ecb700168688450b5981abed74778f3c9cb23160
spennyp <hello@spencerperkins.me>
2021-12-22 01:28:41 -0800

    added schema for user, transactions, liqqudation, user markets

--- src/mapping/ctoken.ts
+++ src/mapping/ctoken.ts
@@ -7,6 +7,7 @@ import {
     AccrueInterest as AccrueInterestEventOld,
     AccrueInterest as AccrueInterestEvent,
     NewReserveFactor as NewReserveFactorEvent,
+    Transfer as TransferEvent,
 } from "../../generated/templates/cToken/cToken";
 
 import { updateMarket } from "../mapping-helpers/market";
@@ -18,7 +19,6 @@ import {
     updateProtocolWeekData,
 } from "../mapping-helpers/historical-data";
 
-export function handleMint(event: MintEvent): void {}
 
 /**
  * This interface for accrue interest was used for ETH, USDC, WBTC, ZRX and BATT
@@ -48,12 +48,38 @@ export function handleAccrueInterest(event: AccrueInterestEvent): void {
     updateProtocolWeekData(event);
 }
 
+/**
+ * Emitted when a user supplies a token to a market
+ * cTokens are minted and given to the supplier 
+ * Also emits a transfer right after
+ */
+export function handleMint(event: MintEvent): void {
+
+}
+
+/**
+ * Emitted when a user redeems cTokens for the underlying token
+ * Also emits a transfer event right before
+ */
 export function handleRedeem(event: RedeemEvent): void {}
 
+/**
+ * Emitted when a user borrows a token 
+ */
 export function handleBorrow(event: BorrowEvent): void {}
 
+/**
+ * Emmiteed when a user repays their borrow 
+ */
 export function handleRepayBorrow(event: RepayBorrowEvent): void {}
 
+/**
+ * Emitted when a user is liquidated
+ * @param LiquidateBorrowEvent.borrower The borrower of this cToken to be liquidated
+ * @param LiquidateBorrowEvent.liqidator The address repaying the borrow and seizing collateral
+ * @param LiquidateBorrowEvent.cTokenCollateral The market in which to seize collateral from the borrower
+ * @param LiquidateBorrowEvent.repayAmount The amount of the underlying borrowed asset to repay
+ */
 export function handleLiquidateBorrow(event: LiquidateBorrowEvent): void {}
 
-export function handleNewReserveFactor(event: NewReserveFactorEvent): void {}
+export function handleTransfer(event: TransferEvent): void {}
